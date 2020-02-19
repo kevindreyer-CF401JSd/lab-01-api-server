d5f7f632ebea297667295682cf8d8f60b765aae9
AstroKD <kdreyerinseattle@gmail.com>
2020-02-18 19:12:39 -0800

    server working author get post put and delete

--- __test__/server.test.js
+++ __test__/server.test.js
@@ -4,12 +4,32 @@ const supertest = require('supertest');
 const mockRequest = supertest(server);
 
 describe('the server', () => {
-    it('responds correcly on GET request to /authors', () => {
+    it('responds on GET request to /authors', () => {
         return mockRequest
             .get('/authors')
             .then(results => {
                 expect(results.status).toBe(200);
             })
-            .catch(console.error)
+    })
+
+    it('adds item on POST to /authors', () => {
+        const newAuthor = { name: 'new POST author'};
+        return mockRequest
+            .post('/authors')
+            .send(newAuthor)
+            .then(results => {
+                expect(results.status).toBe(201);
+                expect(results.body.name).toEqual('new POST author');
+            })
+    })
+    it('updates item with PUT to /authors', () => {
+        const newAuthor = { name: 'new PUT author'};
+        return mockRequest
+            .put('/authors')
+            .send(newAuthor)
+            .then(results => {
+                expect(results.status).toBe(201);
+                expect(results.body.name).toEqual('new PUT author');
+            })
     })
 })
